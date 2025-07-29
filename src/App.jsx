aa034484251a2f2b3262c8105196c037a5856f70
Edoardo Lova <edoardolova81@gmail.com>
2025-07-29 11:36:50 +0200

    chore: init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_SOME_KEY)
+  return (
+    <>
+      <div className="container">
+        <h1>BOOLFLIX</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
