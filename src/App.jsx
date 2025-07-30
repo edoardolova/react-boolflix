bc392f52511e9ef0971e711523abf2352bba89ed
Edoardo Lova <edoardolova81@gmail.com>
2025-07-30 10:29:16 +0200

    feat: add language to country map and flags fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from 'react'
 import WorldFlag from 'react-world-flags';
 import LocaleCodes from 'locale-codes'
+import langToCountries from './languageDataMap';
 
 
 import './App.css'
-import localeCodes from 'locale-codes';
 
 function App() {
   const [search, setSearch] = useState('')
@@ -16,21 +16,28 @@ function App() {
   function handleSearch(){
     fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${search}`)
     .then(res => res.json())
-    .then(data => setFilms(data.results))
+    .then(data =>{
+       setFilms(data.results)
+      })
     .catch(err => console.error(err));
 
     fetch(`https://api.themoviedb.org/3/search/tv?api_key=${apiKey}&query=${search}`)
     .then(res => res.json())
-    .then(data => setSeries(data.results))
+    .then(data => {
+
+      setSeries(data.results)
+    })
     .catch(err => console.error(err));
   }
 
-  // function getFlag(language){
-  //   const stateCode = LocaleCodes.all.find((element)=> element.tag === language)['iso639-2'];
-  //   return(
-  //     <WorldFlag code={stateCode} fallback={<span>{stateCode}</span>} style={{ width: 30, height: 20 }}></WorldFlag>
-  //   )
-  // }
+  function getFlag(language){
+    const countries = langToCountries[language];
+    const stateCode = countries?.[0]
+
+    return(
+      <WorldFlag code={stateCode} fallback={<span>{stateCode}</span>} style={{ width: 30, height: 20 }}></WorldFlag>
+    )
+  }
   function getStars(vote) {
     const fullStars = Math.round(vote / 2);
     const emptyStars = 5 - fullStars;
@@ -68,8 +75,7 @@ function App() {
                   <img src={`https://image.tmdb.org/t/p/w342/${film.poster_path}`}  alt={film.original_title} />
                   <p>{`Titolo: ${film.title}`}</p>
                   <p>{`Titolo originale: ${film.original_title}`}</p>
-                  <p>Lingua: <WorldFlag code={film.original_language} fallback={<span>{film.original_language}</span>} style={{ width: 30, height: 20 }}/></p>
-                  {/* <p>Lingua: {getFlag(film.original_language)}</p> */}
+                  <p>Lingua: {getFlag(film.original_language)}</p>
                   <p>Voto: {getStars(film.vote_average)}</p>
                 </li>
             ))}
@@ -84,7 +90,7 @@ function App() {
                   <img src={`https://image.tmdb.org/t/p/w342/${serie.poster_path}`}  alt={serie.original_name} />
                   <p>{`Titolo: ${serie.name}`}</p>
                   <p>{`Titolo originale: ${serie.original_name}`}</p>
-                  <p>Lingua: <WorldFlag code={serie.original_language} fallback={<span>{serie.original_language}</span>} style={{ width: 30, height: 20 }}/></p>
+                  <p>Lingua: {getFlag(serie.original_language)}</p>
                   <p>Voto: {getStars(serie.vote_average)}</p>
                 </li>
             ))}
---
+++ src/languageDataMap.js
@@ -0,0 +1,78 @@
+const langToCountries = {
+  af: ['ZA'],             // Afrikaans - South Africa
+  am: ['ET'],             // Amharic - Ethiopia
+  ar: ['SA', 'EG', 'IQ', 'JO', 'LB', 'MA', 'AE', 'SY', 'YE', 'TN', 'DZ'],
+  az: ['AZ'],             // Azerbaijani - Azerbaijan
+  be: ['BY'],             // Belarusian - Belarus
+  bg: ['BG'],             // Bulgarian - Bulgaria
+  bn: ['BD', 'IN'],       // Bengali - Bangladesh, India
+  bs: ['BA'],             // Bosnian - Bosnia and Herzegovina
+  ca: ['ES'],             // Catalan - Spain
+  cs: ['CZ'],             // Czech - Czech Republic
+  da: ['DK'],             // Danish - Denmark
+  de: ['DE', 'AT', 'CH', 'LI', 'LU'], // German
+  el: ['GR', 'CY'],       // Greek - Greece, Cyprus
+  en: ['US', 'GB', 'CA', 'AU', 'NZ', 'IE', 'ZA'], // English
+  es: ['ES', 'MX', 'AR', 'CO', 'CL', 'PE', 'VE', 'EC', 'GT', 'CU', 'DO', 'HN', 'PY', 'SV', 'NI', 'CR', 'UY', 'PA', 'BO'],
+  et: ['EE'],             // Estonian - Estonia
+  fa: ['IR'],             // Persian (Farsi) - Iran
+  fi: ['FI'],             // Finnish - Finland
+  fil: ['PH'],            // Filipino - Philippines
+  fr: ['FR', 'BE', 'CH', 'CA', 'LU', 'CM', 'SN', 'CI', 'HT', 'MG'],
+  ga: ['IE'],             // Irish - Ireland
+  gl: ['ES'],             // Galician - Spain
+  gu: ['IN'],             // Gujarati - India
+  he: ['IL'],             // Hebrew - Israel
+  hi: ['IN'],             // Hindi - India
+  hr: ['HR'],             // Croatian - Croatia
+  hu: ['HU'],             // Hungarian - Hungary
+  hy: ['AM'],             // Armenian - Armenia
+  id: ['ID'],             // Indonesian - Indonesia
+  is: ['IS'],             // Icelandic - Iceland
+  it: ['IT', 'CH', 'SM'], // Italian - Italy, Switzerland, San Marino
+  ja: ['JP'],             // Japanese - Japan
+  jv: ['ID'],             // Javanese - Indonesia
+  ka: ['GE'],             // Georgian - Georgia
+  kk: ['KZ'],             // Kazakh - Kazakhstan
+  km: ['KH'],             // Khmer - Cambodia
+  kn: ['IN'],             // Kannada - India
+  ko: ['KR'],             // Korean - South Korea
+  ky: ['KG'],             // Kyrgyz - Kyrgyzstan
+  lo: ['LA'],             // Lao - Laos
+  lt: ['LT'],             // Lithuanian - Lithuania
+  lv: ['LV'],             // Latvian - Latvia
+  mk: ['MK'],             // Macedonian - North Macedonia
+  ml: ['IN'],             // Malayalam - India
+  mn: ['MN'],             // Mongolian - Mongolia
+  mr: ['IN'],             // Marathi - India
+  ms: ['MY', 'SG', 'BN'], // Malay - Malaysia, Singapore, Brunei
+  mt: ['MT'],             // Maltese - Malta
+  nb: ['NO'],             // Norwegian Bokmål - Norway
+  nl: ['NL', 'BE', 'SR'], // Dutch - Netherlands, Belgium, Suriname
+  nn: ['NO'],             // Norwegian Nynorsk - Norway
+  pa: ['IN', 'PK'],       // Punjabi - India, Pakistan
+  pl: ['PL'],             // Polish - Poland
+  ps: ['AF'],             // Pashto - Afghanistan
+  pt: ['PT', 'BR', 'MZ', 'AO'], // Portuguese
+  ro: ['RO', 'MD'],       // Romanian - Romania, Moldova
+  ru: ['RU', 'BY', 'KZ', 'KG'], // Russian
+  si: ['LK'],             // Sinhala - Sri Lanka
+  sk: ['SK'],             // Slovak - Slovakia
+  sl: ['SI'],             // Slovene - Slovenia
+  sq: ['AL'],             // Albanian - Albania
+  sr: ['RS', 'BA', 'ME'], // Serbian
+  sv: ['SE', 'FI'],       // Swedish - Sweden, Finland
+  sw: ['KE', 'TZ', 'UG'], // Swahili - East Africa
+  ta: ['IN', 'LK', 'SG'], // Tamil
+  te: ['IN'],             // Telugu - India
+  th: ['TH'],             // Thai - Thailand
+  tr: ['TR', 'CY'],       // Turkish
+  uk: ['UA'],             // Ukrainian - Ukraine
+  ur: ['PK', 'IN'],       // Urdu - Pakistan, India
+  uz: ['UZ'],             // Uzbek - Uzbekistan
+  vi: ['VN'],             // Vietnamese - Vietnam
+  zh: ['CN', 'SG', 'TW', 'HK', 'MO'], // Chinese
+  zu: ['ZA']              // Zulu - South Africa
+};
+
+export default langToCountries;
